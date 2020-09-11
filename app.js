ad1428df97d3a7532eae8587c1e6abbecd094bd2
Anastasiia Onyshchenko <a.onyshchenko@aimprosoft.com>
2020-09-11 18:15:49 +0300

    create block by js

--- app.js
+++ app.js
@@ -1,25 +1,75 @@
 let long;
 let lat;
 
-let iconElem = document.querySelector('#icon');
-// создаю элементы
+
+let navElemS = Array.from(document.querySelectorAll('.menu li'));
+//беру элеменьты из DOM
 let weatherSection = document.querySelector('.weather');
-let divWeatherBlock = document.createElement('div');
-divWeatherBlock.className = "weather-block";
+let iconElem = document.getElementById('icon');
+let weatherDegreeNum= document.querySelector('.weather-degree_num');
+// let weatherDegreeSpan = document.querySelector('.weather-degree span');
+let weatherDescription = document.querySelector('.weather-description');
+    // выбор температуры в чём показывать
+let selectric = document.querySelector('.selectric-button');
+let selectricLabel = document.querySelector('.selectric-label');
+// weatherDegreeSpan.textContent = selectricLabel.textContent;
+let selectricItemsWrapper = document.querySelector('.selectric-items-wrapper');
+let selectricItems = document.querySelectorAll('.selectric-items li');
+
+
+// создаю элементы
+let createWeatherBlock = document.createElement('div');
+createWeatherBlock.className = "weather-block";
 let canvas = document.createElement('canvas');
 canvas.id = "icon";
 canvas.setAttribute('width', '128');
 canvas.setAttribute('height', '128');
-let divWeatherDegree = document.createElement('div');
-divWeatherDegree.className = "weather-degree";
+let createWeatherDegreeWrapper = document.createElement('div');
 let spanWeatherDegree = document.createElement('span');
-divWeatherDegree.append(spanWeatherDegree);
-let divWeatherDescription = document.createElement('div');
-divWeatherDescription.className = "weather-description";
-divWeatherBlock.append(canvas, divWeatherDegree,divWeatherDescription );
+createWeatherDegree.className = "weather-degree";
+let spanWeatherDegreeNum = document.createElement('span');
+spanWeatherDegreeNum.className = "weather-degree_num";
+createWeatherDegreeWrapper.append(spanWeatherDegree);
+let createWeatherDescription = document.createElement('div');
+createWeatherDescription.className = "weather-description";
+createWeatherBlock.append(canvas, createWeatherDegree,createWeatherDescription );
 
-function checkDataDay(day){
+
+//клик по выборы температуры
+function showDegreesVariants(){
+    selectricItemsWrapper.style.display = 'block';
+}
+//ыбор градусов
+function chooseDegrees(e){
+    selectricItemsWrapper.removeAttribute("style");
+    // weatherDegreeSpan.textContent = e.textContent;
+    selectricLabel = e.textContent;
+
+}
+//клик по меню
+function checkDataDay(nav, day){
+    changeActiveNav (nav);
     console.log(day);
+/*
+    switch (day) {
+        case 'today':
+            main.innerHTML = todayTemplate;
+            break;
+        case 'tomorrow':
+            main.innerHTML = todayTemplate;
+            break;
+        case "threeDays":
+            main.innerHTML = "";
+            break;
+        case 'week':
+            main.innerHTML = "";
+            break;
+    }
+*/
+}
+function changeActiveNav (nav){
+    navElemS.map(el => el.classList.remove('active'));
+    nav.classList.add('active');
 }
 function checkGeolocation(){
     navigator.geolocation ? getCoords() : h1.textContent = 'unlock your location';
@@ -40,13 +90,15 @@ async function getWeatherAPI(long, lat){
 }
 function addContent(data){
     const {temperature, summary, icon} = data.currently;
+    weatherDegreeNumtextContent = temperature;
+    weatherDescription.textContent = summary;
 
     setIcons(icon, iconElem);
     createBlockTemplate()
 }
 function createBlockTemplate() {
 
-    weatherSection.append(divWeatherBlock);
+    weatherSection.append(createWeatherBlock);
 
 }
 function setIcons(icon, iconID) {
--- import/old.js
+++ import/old.js
@@ -1,70 +1,109 @@
+let todayTemplate = '<div class="location"><h1 class="location-timezone v2">Timezone</h1><canvas id="icon" width="128" height="128">Icon</canvas></div><div class="temperature temperature-section"><div class="degree-section"><h2 class="temperature-degree"></h2><span>˚F, mph</span></div><div class="temperature-description">Comfortable weather</div></div>';
+let threeDaysTemplate = '<div class="temperature temperature-section" data-block><div class="location"><canvas id="icon" class="icon" width="128" height="128">Icon</canvas></div> <div class="degree-section"><h2 class="temperature-degree"></h2></div> <div class="temperature-description">Comfortable weather</div></div>'
+let threeDaysTemplate2 = '<div class="temperature temperature-section"><div class="location"><canvas id="icon" class="icon" width="128" height="128">Icon</canvas></div> <div class="degree-section"><h2 class="temperature-degree"></h2></div> <div class="temperature-description">Comfortable weather</div></div>'
 
-window.addEventListener('load', () => {
+let main = document.querySelector('#main');
 
+function showContent(data) {
+    switch (data.dataset.day) {
+        case 'today':
+            main.innerHTML = todayTemplate;
+            break;
+        case 'tomorrow':
+            main.innerHTML = todayTemplate;
+            break;
+        case "threeDays":
+            main.innerHTML = "";
+            break;
+        case 'week':
+            main.innerHTML = "";
+            break;
+    }
+    setTimeout(() => navigation(), 2000);
+}
+
+
+
+// showContent(dataActiveDay);
+
+
+function navigation() {
     let long;
     let lat;
+    let newDegree;
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDesc = document.querySelector('.temperature-description');
     let lacationTimeZone = document.querySelector('.location-timezone');
     let iconId = document.querySelector('#icon');
+    // let iconS =
     let temperatureSection = document.querySelector('.temperature-section');
     let temperatureSpan = document.querySelector('.temperature-section span');
     let selectric = document.querySelector('.selectric-button');
     let selectricLabel = document.querySelector('.selectric-label');
     let selectricItemsWrapper = document.querySelector('.selectric-items-wrapper');
-    let main = document.querySelector('#main');
     let selectricItems = document.querySelectorAll('.selectric-items li');
-    let dataActiveDays = document.querySelector('[data-day].active');
+    let menu = Array.from(document.querySelectorAll('.menu li'));
+    let dataActiveDay = document.querySelector('[data-day].active');
 
 
-    let today = '<div class="location"><h1 class="location-timezone">Timezone</h1><canvas id="icon" width="128" height="128">Icon</canvas></div><div class="temperature temperature-section"><div class="degree-section"><h2 class="temperature-degree">24</h2><span>˚F, mph</span></div><div class="temperature-description">Comfortable weather</div></div>';
-    console.log(dataActiveDays.dataset.days);
-
-    function showContent(){
-        console.log(main);
-        switch (dataActiveDays.dataset.days){
-            case 'today':
-                main.innerHTML = today;
-                break;
-            case 'tomorrow':
-                main.textContent = "tomorrow";
-                break;
-            case "10 days":
-                main.textContent = "10 days";
-                break;
-            case 'month':
-                main.textContent = "month";
-                break;
-            default:
-                main.textContent = today;
-        }
-    }
-
-
-    showContent();
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const proxy = "https://cors-anywhere.herokuapp.com/"; // чтобы локально достучаться
 
-            // const proxy = "https://cors-anywhere.herokuapp.com/";
             let url = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-            async function getWeatherAPI() {
-                const response = await fetch(`${proxy}${url}`);
-                const data = await response.json();
-                console.log(url);
-                const {temperature, summary, icon} = data.currently;
 
+
+            function setContentMoreeDays(data, days) {
+                let threeDaysArr = data.daily.data.slice(0,days);
+                threeDaysArr.forEach(el => {
+                    let {summary, icon, temperatureHigh} = el;
+                    main.insertAdjacentHTML("beforeEnd" , threeDaysTemplate);
+                    // temperatureSection.dataset.block = `${i}`;
+                    // let temperatureSection = document.querySelector('.temperature-section');
+                    // let temperatureDegree2 = temperatureSectionS[i].querySelector('.temperature-degree');
+                    // let temperatureDesc2 = document.querySelector('.temperature-description');
+                    console.log(summary, icon, temperatureHigh);
+                    // temperatureDegree2.textContent = temperatureHigh;
+                    // temperatureDesc2.textContent = summary;
+                    // setIcons(icon, iconId);
+                });
+                let temperatureSectionS = Array.from(document.querySelectorAll('.temperature-section'));
+                console.log(temperatureSectionS);
+                for (let i = 0; i < temperatureSectionS.length; i++){
+                    temperatureSectionS[i].dataset.block = `${i}`;
+                    console.log(threeDaysArr[i]);
+                    let {summary, icon, temperatureHigh} = threeDaysArr[i];
+
+                    let temperatureDegree = temperatureSectionS[i].querySelector('.temperature-degree');
+                    let temperatureDesc = temperatureSectionS[i].querySelector('.temperature-description');
+
+                    temperatureDegree.textContent = temperatureHigh;
+                    temperatureDesc.textContent = summary;
+                }
+            }
+            function setTextContent(data){
+                // let summary, icon, temperature;
+                if (dataActiveDay.dataset.day === 'tomorrow') {
+                    var {summary, icon} = data.daily;
+                    var temperature = data.daily.data[0].temperatureHigh
+                }else if (dataActiveDay.dataset.day === 'today'){
+                    var {summary, icon, temperature} = data.currently;
+                }
                 //Set DOM Elements from the API
                 temperatureDegree.textContent = temperature;
                 temperatureDesc.textContent = summary;
                 lacationTimeZone.textContent = data.timezone;
                 //Set Icon
                 setIcons(icon, iconId);
-                console.log(typeof temperature);
-                console.log(temperature - 20);
-                let newDegree = (temperature - 32) * (5 / 9);
+                console.log(temperature - 20);                newDegree = (temperature - 32) * (5 / 9);
+            }
+
+            async function getWeatherAPI() {
+                const response = await fetch(`${proxy}${url}`);
+                const data = await response.json();
+                (dataActiveDay.dataset.day === 'threeDays' || dataActiveDay.dataset.day === 'week') ?  setContentMoreeDays(data,  dataActiveDay.dataset.quantity) : setTextContent(data);
 
                 //chancge temperature to  C/F
                 selectric.addEventListener('click', () => {
@@ -74,15 +113,30 @@ window.addEventListener('load', () => {
                     el.addEventListener('click', () => {
                         console.log(el.textContent);
                         selectricLabel.textContent = el.textContent;
-                        temperatureSpan.textContent =el.textContent;
+                        temperatureSpan.textContent = el.textContent;
                         selectricItemsWrapper.removeAttribute("style");
                         if (el.dataset.index === "0") {
                             temperatureDegree.textContent = temperature;
-                        }else {
+                        } else {
                             temperatureDegree.textContent = Math.floor(newDegree);
                         }
                     });
                 });
+
+                //change days to show
+                menu.forEach(el => {
+                    el.addEventListener('click', () => {
+                        changeActiveEl(dataActiveDay, el);
+                    })
+                });
+
+                function changeActiveEl(active, ChangeToActive) {
+                    active.classList.remove('active');
+                    ChangeToActive.classList.add('active');
+                    let newData = document.querySelector('[data-day].active');
+                    showContent(newData);
+                }
+
                 // temperatureSection.addEventListener('click', () => {
                 //    if (temperatureSpan.textContent === "F") {
                 //         temperatureSpan.textContent = "C";
@@ -93,12 +147,12 @@ window.addEventListener('load', () => {
                 //    }
                 // })
             }
-            getWeatherAPI();
 
+            getWeatherAPI();
 
         });
 
-    }else{
+    } else {
         h1.textContent = 'unlock your location'
     }
 
@@ -109,8 +163,9 @@ window.addEventListener('load', () => {
         console.log(CurrentIcon);
         return skycons.set(iconID, Skycons[CurrentIcon]);
     }
+}
+window.addEventListener('load', () => {
 
-    // import {Today} from './import/today.js';
-    // const { today } = require('./import/today.js');
-    console.log(today)
+    navigation();
+
 });
